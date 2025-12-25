b50fccf89731536e67d178ae3d24bbcbd49ea568
mp1707 <marco.preuss1707@gmail.com>
2025-12-25 01:26:02 +0100

    reset

--- App.tsx
+++ App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { View } from "react-native";
-import { DiceTray3D } from "./src/dice/DiceTray3D";
-import { GamePanel } from "./src/ui/GamePanel";
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <DiceTray3D />
-      </View>
-      <View style={{ flex: 1 }}>
-        <GamePanel />
-      </View>
+      <View style={{ flex: 1 }}>{/* <DiceTray3D /> */}</View>
+      <View style={{ flex: 1 }}>{/* <GamePanel /> */}</View>
     </View>
   );
 }
