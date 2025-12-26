7d19313d7676d9b1d8a50dffb0bcb7e24f660534
mp1707 <marco.preuss1707@gmail.com>
2025-12-26 10:52:19 +0100

    eas update config

--- app.config.ts
+++ app.config.ts
@@ -1,50 +1,39 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-const APP_VARIANT = process.env.APP_VARIANT as
-  | "development"
-  | "preview"
-  | "production"
-  | undefined;
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
-const getBundleId = (): string => {
-  switch (APP_VARIANT) {
-    case "development":
-      return "com.mp17.mpapps.dicegame.dev";
-    case "preview":
-      return "com.mp17.mpapps.dicegame.preview";
-    case "production":
-    default:
-      return "com.mp17.mpapps.dicegame";
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return "com.mp17.mpapps.dicegame.dev";
   }
+  if (IS_PREVIEW) {
+    return "com.mp17.mpapps.dicegame.preview";
+  }
+  return "com.mp17.mpapps.dicegame";
 };
 
-const getAppName = (): string => {
-  switch (APP_VARIANT) {
-    case "development":
-      return "Dice Game (Dev)";
-    case "preview":
-      return "Dice Game (Preview)";
-    case "production":
-    default:
-      return "Dice Game";
+const getAppName = () => {
+  if (IS_DEV) {
+    return "Dice Game (Dev)";
   }
+  if (IS_PREVIEW) {
+    return "Dice Game (Preview)";
+  }
+  return "Dice Game";
 };
+
+const SCHEME = IS_DEV
+  ? "dicegame-dev"
+  : IS_PREVIEW
+  ? "dicegame-preview"
+  : "dicegame";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "dice-game",
-  extra: {
-    eas: {
-      projectId: "829d31d2-6ecf-43e7-bc3b-af9b4c9cd43b",
-    },
-  },
-  updates: {
-    url: "https://u.expo.dev/829d31d2-6ecf-43e7-bc3b-af9b4c9cd43b",
-  },
-  runtimeVersion: {
-    policy: "appVersion",
-  },
+  scheme: SCHEME,
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -57,7 +46,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   ios: {
     supportsTablet: true,
-    bundleIdentifier: getBundleId(),
+    bundleIdentifier: getUniqueIdentifier(),
+    infoPlist: {
+      CFBundleAllowMixedLocalizations: true,
+    },
   },
   android: {
     adaptiveIcon: {
@@ -65,9 +57,21 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       backgroundColor: "#ffffff",
     },
     edgeToEdgeEnabled: true,
-    package: getBundleId(),
+    package: getUniqueIdentifier(),
   },
   web: {
     favicon: "./assets/favicon.png",
+  },
+  plugins: ["expo-dev-client"],
+  extra: {
+    eas: {
+      projectId: "829d31d2-6ecf-43e7-bc3b-af9b4c9cd43b",
+    },
+  },
+  updates: {
+    url: "https://u.expo.dev/829d31d2-6ecf-43e7-bc3b-af9b4c9cd43b",
+  },
+  runtimeVersion: {
+    policy: "appVersion",
   },
 });
