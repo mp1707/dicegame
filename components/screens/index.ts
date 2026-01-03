78b288275abbdc6f3e23cd9f4dd72a45edbf0647
mp1707 <marco.preuss1707@gmail.com>
2026-01-03 17:08:10 +0100

    cleanup

--- components/screens/index.ts
+++
@@ -1,9 +0,0 @@
-// Screen wrappers (for backwards compatibility)
-export { ShopScreen } from "./ShopScreen";
-export { UpgradePickerScreen } from "./UpgradePickerScreen";
-export { EndScreen } from "./EndScreen";
-
-// Panel components (for PhaseDeck animation - legacy exports)
-export { ShopPanel } from "./ShopScreen";
-export { UpgradePickerPanel } from "./UpgradePickerScreen";
-export { EndPanel } from "./EndScreen";
